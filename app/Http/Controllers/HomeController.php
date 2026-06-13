b2d3ff373f9f7478e0c940ce6d5fadb25f8d19d3
Bayu Prayoga <prayogabayu062@gmail.com>
2026-06-13 21:48:22 +0700

    Fixing Paket

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -56,14 +56,9 @@ class HomeController extends Controller
         $siswalk = Siswa::where('jk', 'L')->count();
         $siswapr = Siswa::where('jk', 'P')->count();
         $kelas = Kelas::count();
-        $bkp = Kelas::where('paket_id', '1')->count();
-        $dpib = Kelas::where('paket_id', '2')->count();
-        $ei = Kelas::where('paket_id', '3')->count();
-        $oi = Kelas::where('paket_id', '4')->count();
-        $tbsm = Kelas::where('paket_id', '6')->count();
-        $rpl = Kelas::where('paket_id', '7')->count();
-        $tpm = Kelas::where('paket_id', '5')->count();
-        $las = Kelas::where('paket_id', '8')->count();
+        $is = Kelas::where('paket_id', '1')->count();
+        $ib = Kelas::where('paket_id', '2')->count();
+        $ia = Kelas::where('paket_id', '3')->count();
         $mapel = Mapel::count();
         $user = User::count();
         $paket = Paket::all();
@@ -76,14 +71,9 @@ class HomeController extends Controller
             'siswapr',
             'siswa',
             'kelas',
-            'bkp',
-            'dpib',
-            'ei',
-            'oi',
-            'tbsm',
-            'rpl',
-            'tpm',
-            'las',
+            'is',
+            'ib',
+            'ia',
             'mapel',
             'user',
             'paket'
